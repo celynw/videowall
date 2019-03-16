95df491ba53a634607c020049f6a9b140dcd7349
Celyn Walters <3299161+celynw@users.noreply.github.com>
2019-03-16 01:31:04 +0000

    Split into class (stream.py), made work for multiple videos

--- videowall.py
+++ videowall.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
+from stream import FileVideoStream
+
 from argparse import ArgumentParser
 from pathlib import Path
 from threading import Thread
 import sys
 import time
 import cv2
-from queue import Queue
-import imutils
-from imutils.video import FPS
+# from queue import Queue
+# import imutils
+# from imutils.video import FPS
 from screeninfo import get_monitors
 from PIL import Image, ImageOps
 import numpy as np
@@ -53,15 +55,40 @@ def play_videos(files, dimensions):
 		shuffle(files)
 		caps = [None] * (dimensions[0] * dimensions[1])
 		frames = [None] * len(caps)
-		retvals = [None] * len(caps)
+		# retvals = [None] * len(caps)
+		lastFrames = [time.time()] * len(caps)
 
 		for i in range(0, min(len(files), len(caps))):
-			caps[i] = cv2.VideoCapture(str(files[i]))
+			# caps[i] = cv2.VideoCapture(str(files[i]))
+			caps[i] = FileVideoStream(str(files[i]), queueSize=800).start()
+
+		time.sleep(1)
+
 
 		while True:
 			for (i, cap) in enumerate(caps):
 				if (cap):
-					retvals[i], frames[i] = cap.read()
+					# retvals[i], frames[i] = cap.read()
+					if cap.more():
+						# grab the frame from the threaded video file stream, resize
+						# it, and convert it to grayscale (while still retaining 3
+						# channels)
+						if ((time.time() - lastFrames[i]) < cap.frameTime):
+							continue
+						frames[i] = cap.read()
+						lastFrames[i] = time.time()
+
+						# frames[i] = imutils.resize(frames[i], width=450)
+
+						# display the size of the queue on the frame
+						# cv2.putText(frames[i], "Queue Size: {}".format(cap.Q.qsize()),
+						# 	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+						# show the frame and update the FPS counter
+						# cv2.imshow("Frame", frames[i])
+						# cv2.waitKey(1)
+						# fps.update()
+
 			# TODO replace with range
 			frames = [np.zeros((1920, 1080, 3), dtype="uint8") if frame is None else frame for frame in frames]
 			show_combined_frames(windowName, dimensions, frames)
@@ -114,115 +141,51 @@ def finish(caps=None, ok=True):
 	if (caps):
 		for cap in caps:
 			if (cap):
-				cap.release()
+				# cap.release()
+				cap.stop()
 	cv2.destroyAllWindows()
 	quit(ok)
 
 
 #===================================================================================================
-class FileVideoStream:
-	#-----------------------------------------------------------------------------------------------
-	def __init__(self, path, queueSize=128):
-		# initialize the file video stream along with the boolean
-		# used to indicate if the thread should be stopped or not
-		self.stream = cv2.VideoCapture(path)
-		self.fps = self.stream.get(cv2.CAP_PROP_FPS)
-		self.frameTime = 1.0 / self.fps
-		self.stopped = False
-
-		# initialize the queue used to store frames read from
-		# the video file
-		self.Q = Queue(maxsize=queueSize)
-
-	#-----------------------------------------------------------------------------------------------
-	def start(self):
-		# start a thread to read frames from the file video stream
-		t = Thread(target=self.update, args=())
-		t.daemon = True
-		t.start()
-		return self
-
-	#-----------------------------------------------------------------------------------------------
-	def update(self):
-		# keep looping infinitely
-		while True:
-			# if the thread indicator variable is set, stop the
-			# thread
-			if self.stopped:
-				return
-
-			# otherwise, ensure the queue has room in it
-			if not self.Q.full():
-				# read the next frame from the file
-				(grabbed, frame) = self.stream.read()
-
-				# if the `grabbed` boolean is `False`, then we have
-				# reached the end of the video file
-				if not grabbed:
-					self.stop()
-					return
-
-			# add the frame to the queue
-			self.Q.put(frame)
-
-	#-----------------------------------------------------------------------------------------------
-	def read(self):
-		# return next frame in the queue
-		return self.Q.get()
-
-	#-----------------------------------------------------------------------------------------------
-	def more(self):
-		# return True if there are still frames in the queue
-		return self.Q.qsize() > 0
-
-	#-----------------------------------------------------------------------------------------------
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
-
-
-#===================================================================================================
 if (__name__=="__main__"):
-	# args = parse_args()
-	# files = get_files(*args._get_kwargs())
-	# play_videos(files, (args.w, args.h))
+	args = parse_args()
+	files = get_files(*args._get_kwargs())
+	play_videos(files, (args.w, args.h))
 
-	print("[INFO] starting video file thread...")
-	fvs = FileVideoStream("D:/test.webm").start()
-	time.sleep(0.5)
+	# fvs = FileVideoStream("D:/test.webm").start()
+	# time.sleep(0.5)
 
 	# start the FPS timer
-	fps = FPS().start()
+	# fps = FPS().start()
 
 	# loop over frames from the video file stream
-	lastFrame = time.time()
-	while fvs.more():
-		# grab the frame from the threaded video file stream, resize
-		# it, and convert it to grayscale (while still retaining 3
-		# channels)
-		if ((time.time() - lastFrame) < fvs.frameTime):
-			continue
-		frame = fvs.read()
-		lastFrame = time.time()
+	# lastFrame = time.time()
+	# while fvs.more():
+	# 	# grab the frame from the threaded video file stream, resize
+	# 	# it, and convert it to grayscale (while still retaining 3
+	# 	# channels)
+	# 	if ((time.time() - lastFrame) < fvs.frameTime):
+	# 		continue
+	# 	frame = fvs.read()
+	# 	lastFrame = time.time()
 
-		frame = imutils.resize(frame, width=450)
-		# frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		# frame = np.dstack([frame, frame, frame])
+	# 	# frame = imutils.resize(frame, width=450)
 
-		# display the size of the queue on the frame
-		cv2.putText(frame, "Queue Size: {}".format(fvs.Q.qsize()),
-			(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+	# 	# display the size of the queue on the frame
+	# 	cv2.putText(frame, "Queue Size: {}".format(fvs.Q.qsize()),
+	# 		(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
-		# show the frame and update the FPS counter
-		cv2.imshow("Frame", frame)
-		cv2.waitKey(1)
-		fps.update()
+	# 	# show the frame and update the FPS counter
+	# 	cv2.imshow("Frame", frame)
+	# 	cv2.waitKey(1)
+	# 	fps.update()
 
 	# stop the timer and display FPS information
-	fps.stop()
-	print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	# fps.stop()
+	# print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+	# print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 	# do a bit of cleanup
-	cv2.destroyAllWindows()
-	fvs.stop()
+	# cv2.destroyAllWindows()
+	# fvs.stop()
