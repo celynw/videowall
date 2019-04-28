2b62c345221502f5a7a8ea7ebb58d77a1b82dd15
Celyn Walters <3299161+celynw@users.noreply.github.com>
2019-04-28 23:48:02 +0100

    NEW DIRECTION, better performance: Use vlclib in PyQt5 frames

--- videowall.py
+++ videowall.py
@@ -1,16 +1,105 @@
 #!/usr/bin/env python
-from stream import FileVideoStream
-
 from argparse import ArgumentParser
 from pathlib import Path
-from threading import Thread
-import sys
-import time
-import cv2
-from screeninfo import get_monitors
-from PIL import Image
-import numpy as np
+from PyQt5 import QtCore, QtWidgets
 from random import shuffle
+import sys
+import vlc
+
+#===================================================================================================
+class Frame():
+	#-----------------------------------------------------------------------------------------------
+	def __init__(self, widgetParent, mouseDoubleClickEvent, path):
+		self.videoFrame = QtWidgets.QFrame()
+		widgetParent.addWidget(self.videoFrame)
+		self.videoFrame.mouseDoubleClickEvent = mouseDoubleClickEvent
+		# Cannot loop indefinitely (e.g. =-1)
+		self.vlcInstance = vlc.Instance(['--video-on-top', "--input-repeat=65535"])
+		self.videoPlayer = self.vlcInstance.media_player_new()
+		self.videoPlayer.video_set_mouse_input(True)
+		self.videoPlayer.video_set_key_input(True)
+		# self.videoPlayer.audio_set_mute(True)
+		self.videoPlayer.set_media(self.vlcInstance.media_new(str(path)))
+		if sys.platform.startswith('linux'): # for Linux using the X Server
+			self.videoPlayer.set_xwindow(self.videoFrame.winId())
+		elif sys.platform == "win32": # for Windows
+			self.videoPlayer.set_hwnd(self.videoFrame.winId())
+		elif sys.platform == "darwin": # for MacOS
+			self.videoPlayer.set_nsobject(int(self.videoFrame.winId()))
+
+		self.videoPlayer.play()
+
+
+#===================================================================================================
+class MainWindow(QtWidgets.QMainWindow):
+	#-----------------------------------------------------------------------------------------------
+	def __init__(self, files, size, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		shuffle(files)
+		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+		self.move(0, 0)
+		self.resize(1920, 1080)
+		self.show()
+		self.mainFrame = QtWidgets.QFrame()
+		self.setCentralWidget(self.mainFrame)
+		layout = QtWidgets.QVBoxLayout()
+		layout.setContentsMargins(0, 0, 0, 0)
+		layout.setSpacing(0)
+
+		file = 0
+		for row in range(size[1]):
+			layoutRow = QtWidgets.QHBoxLayout()
+			layoutRow.setContentsMargins(0, 0, 0, 0)
+			for col in range(size[0]):
+				file += 1
+				if (file < len(files)):
+					Frame(layoutRow, self.mouseDoubleClickEvent, files[file])
+			layout.addLayout(layoutRow)
+		self.mainFrame.setLayout(layout)
+		self.show()
+
+	#-----------------------------------------------------------------------------------------------
+	def mouseDoubleClickEvent(self, event):
+		if event.button() == Qt.LeftButton:
+			if self.windowState() == Qt.WindowNoState:
+				self.videoFrame1.hide()
+				self.videoFrame.show()
+				self.setWindowState(Qt.WindowFullScreen)
+			else:
+				self.videoFrame1.show()
+				self.setWindowState(Qt.WindowNoState)
+
+	#-----------------------------------------------------------------------------------------------
+	def mouseDoubleClickEvent1(self, event):
+		if event.button() == Qt.LeftButton:
+			if self.windowState() == Qt.WindowNoState:
+				self.videoFrame.hide()
+				self.videoFrame1.show()
+				self.setWindowState(Qt.WindowFullScreen)
+			else:
+				self.videoFrame.show()
+				self.setWindowState(Qt.WindowNoState)
+
+
+#===================================================================================================
+def get_files(*args):
+	args = dict(args)
+	root = Path(args["root"])
+	if not root.exists():
+		print(f"Directory '{root}' not found")
+		quit(1)
+
+	filetypes = [
+		"*.mp4",
+		"*.webm"
+	]
+	allFiles = []
+	for filetype in filetypes:
+		files = root.glob(filetype)
+		allFiles.extend(files)
+
+	return allFiles
+
 
 #===================================================================================================
 def parse_args():
@@ -23,104 +112,10 @@ def parse_args():
 
 
 #===================================================================================================
-def get_files(*args):
-	args = dict(args)
-	root = Path(args["root"])
-	if not root.exists():
-		print("Directory '{}' not found".format(root))
-		quit(1)
-
-	filetypes = [
-			"*.mp4",
-			"*.webm"]
-	allFiles = []
-	for filetype in filetypes:
-		files = root.glob(filetype)
-		allFiles.extend(files)
-
-	return allFiles
-
-
-#===================================================================================================
-def play_videos(files, dimensions):
-	windowName = "Video Wall"
-	window = cv2.namedWindow(windowName, cv2.WND_PROP_FULLSCREEN)
-	cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-	m = get_monitors()[0]
-	targetSize = (m.width // dimensions[0], m.height // dimensions[1])
-
-	while True:
-		# Randomise on each iteration
-		shuffle(files)
-		caps = [None] * (dimensions[0] * dimensions[1])
-		frames = [Image.new("RGB", (targetSize[0], targetSize[1]))] * len(caps)
-		lastFrames = [time.time()] * len(caps)
-
-		for i in range(0, min(len(files), len(caps))):
-			caps[i] = FileVideoStream(str(files[i]), targetSize, queueSize=800, loop=True).start()
-		time.sleep(1)
-
-		while True:
-			for (i, cap) in enumerate(caps):
-				if (cap):
-					if (cap.more()):
-						if ((time.time() - lastFrames[i]) < cap.frameTime):
-							continue
-						frames[i] = cap.read()
-						lastFrames[i] = time.time()
-			show_combined_frames(windowName, dimensions, frames)
-
-			key = cv2.waitKey(1)
-			if (key == ord('q')):
-				finish(caps)
-			elif (key == ord('r')):
-				stop_caps(caps)
-				break
-
-	finish(ok=False)
-
-
-#===================================================================================================
-def show_combined_frames(windowName, dimensions, frames):
-	"""
-	Combine frames of same dimensions into a grid.
-	Will run on main display.
-	"""
-
-	layout = list(chunks(frames, dimensions[0]))
-
-	rows = [None] * dimensions[1]
-	for i, row in enumerate(layout):
-		rows[i] = np.hstack(row)
-	full = np.vstack(rows)
-
-	cv2.imshow(windowName, full)
-
-
-#===================================================================================================
-def chunks(list, numElements):
-	for i in range(0, len(list), numElements):
-		yield list[i:i + numElements]
-
-
-#===================================================================================================
-def stop_caps(caps):
-	for cap in caps:
-		if (cap):
-			cap.stop()
-
-
-#===================================================================================================
-def finish(caps=None, ok=True):
-	if (caps):
-		stop_caps(caps)
-	cv2.destroyAllWindows()
-	quit(ok)
-
-
-#===================================================================================================
-if (__name__=="__main__"):
+if (__name__ == "__main__"):
 	args = parse_args()
 	files = get_files(*args._get_kwargs())
-	play_videos(files, (args.w, args.h))
+	app = QtWidgets.QApplication([])
+	app.setApplicationName("Video Wall")
+	window = MainWindow(files, (args.w, args.h))
+	app.exec_()
